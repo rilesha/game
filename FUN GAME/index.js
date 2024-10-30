6890cb0e7e4acedcb07e04d46c730f2a50ca14ed
rilesha <146503843+rilesha@users.noreply.github.com>
2024-10-30 15:43:03 +0545

    Add files via upload

---
+++ FUN GAME/index.js
@@ -0,0 +1,21 @@
+let image=document.getElementById("image")
+let msg = document.getElementById("message");
+let  userName;
+ document.getElementById("btn").onclick = function(){
+userName = document.getElementById("name").value;
+if(userName==="rilesh"){
+  msg.innerText = `you are gay ${userName}`;
+}
+else if(userName==="Rilesh"){
+  msg.innerText = `you are gay ${userName}`;
+  
+}
+else{
+  msg.innerText = "ENTER YOUR FUCKING NAME DUMBASS"
+}
+ }
+
+
+
+
+
